4993ec8bea1e079d6e01ead5c3b8df6f6910561a
Uwe Jugel <d049428@DRSN00258224A.(none)>
2011-10-26 17:14:45 +0200

    adding cmd script removed comment from sh

---
+++ mm2wiki.cmd
@@ -0,0 +1,4 @@
+@ECHO OFF
+REM copy mm2wiki.js to an appropriate path before using this cmd file
+
+node lib/mm2wiki.js mm2wiki %*
--- mm2wiki.sh
+++ mm2wiki.sh
@@ -3,6 +3,3 @@
 file=$1
 
 node lib/mm2wiki.js mm2wiki $file
-
-# node -e "require('./lib/mm2wiki.js').convert('$1', function(err, result) { console.log(result); });"
-
